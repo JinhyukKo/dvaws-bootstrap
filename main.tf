0685c751ae17bc43ae1478f3f731f5196709eb3f
JinHyukKo <kobin1970@gmail.com>
2026-05-01 23:23:33 +0900

    add iam role

--- main.tf
+++ main.tf
@@ -1,5 +1,10 @@
+resource "random_string" "suffix" {
+  length  = 6
+  upper   = false
+  special = false
+}
 resource "aws_s3_bucket" "tf_state" {
-  bucket = var.state_bucket_name
+  bucket = "${var.state_bucket_name}-${random_string.suffix.result}"
   tags   = var.tags
   lifecycle {
     prevent_destroy = true
@@ -24,6 +29,17 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "tf_state" {
   }
 }
 
+
+
+resource "aws_dynamodb_table" "tf_lock" {
+  name         = var.lock_table_name
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
 resource "aws_s3_bucket_public_access_block" "tf_state" {
   bucket                  = aws_s3_bucket.tf_state.id
   block_public_acls       = true
