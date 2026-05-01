c92906d24017850c5a4fb84455ebee3ef9747f55
JinHyukKo <kobin1970@gmail.com>
2026-05-02 00:03:41 +0900

    add outputs

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,9 @@ output "lock_table_name" {
   description = "Name of the DynamoDB lock table."
   value       = aws_dynamodb_table.tf_lock.name
 }
+
+output "terraform_state_role_arn" {
+  description = "ARN of the Terraform state role."
+  value       = aws_iam_role.terraform_state_role.arn
+}
+
